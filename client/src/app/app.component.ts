d6ce7eef221a91a803e7619c62c8aa34753d5887
ashwinmsad1 <ashwinmsad@gmail.com>
2025-02-05 19:27:48 +0530

    End of section 3

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { NgFor } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [NgFor],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent implements OnInit {
+  http = inject(HttpClient);
+  title = 'Dating App';
+  users: any;
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:5000/api/users').subscribe({
+      next: (response) => (this.users = response),
+      error: (error) => console.log(error),
+      complete: () => console.log('Request has completed'),
+    });
+  }
+}
