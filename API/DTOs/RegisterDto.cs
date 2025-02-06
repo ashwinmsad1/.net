32bc03096e8eaf410b3b3c0fd2c5d75ec1999c0e
ashwinmsad1 <ashwinmsad@gmail.com>
2025-02-06 10:29:01 +0530

    End of Section 4

---
+++ API/DTOs/RegisterDto.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Net.Http.Headers;
+
+namespace API.DTOs;
+
+public class RegisterDto
+{
+    [Required]
+    public required string Username { get; set; }
+
+    [Required]
+    public required string Password { get; set; }
+}
